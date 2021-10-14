c48a44f4923fa62a1fb11d729828ecd4ee635b3e
Ahmed Saleh <a.s.alsalali@gmail.com>
2021-10-14 19:20:02 +0800

    updated boostrapping the application

--- main.go
+++ main.go
@@ -4,23 +4,31 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ahmed-saleh/playbook/config"
+	"github.com/ahmed-saleh/playbook/models"
 	"github.com/ahmed-saleh/playbook/routers"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
-
+	config.Setup()
+	models.Setup()
 }
 
 func main() {
+	gin.SetMode(config.ServerSetting.RunMode)
 	routersInit := routers.InitRouter()
+	readTimeout := config.ServerSetting.ReadTimeout
+	writeTimeout := config.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", config.ServerSetting.HttpPort)
 	maxHeaderBytes := 1 << 20
-	endPoint := fmt.Sprintf(":%d", 8080) //ini this
 
 	server := &http.Server{
 		Handler:        routersInit,
 		Addr:           endPoint,
 		MaxHeaderBytes: maxHeaderBytes,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
 	}
-
 	server.ListenAndServe()
 }
