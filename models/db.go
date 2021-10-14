baefe1cab702e2c9037385d4a58c86e98bd1948f
Ahmed Saleh <a.s.alsalali@gmail.com>
2021-10-14 19:19:33 +0800

    added sample models

---
+++ models/db.go
@@ -0,0 +1,41 @@
+/*
+	This package is the base model for all gorm objects
+*/
+package models
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ahmed-saleh/playbook/config"
+	"github.com/google/uuid"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	ID        uint `gorm:"primaryKey,autoIncrement"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	uuid      uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4()"`
+}
+
+func Setup() {
+
+	var err error
+	//TODO: clean up this
+	dsn := config.MysqlSettings.User + ":" + config.MysqlSettings.Password + "@tcp" + "(" + config.MysqlSettings.Host + ":" + config.MysqlSettings.Port + ")/" + config.MysqlSettings.Name + "?" + "parseTime=true&loc=Local"
+	fmt.Println(dsn)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("models.Setup err: %v", err)
+	}
+
+	db.AutoMigrate(&Model{}, &User{})
+}
---
+++ models/user.go
@@ -0,0 +1,10 @@
+/*
+	User model
+*/
+package models
+
+type User struct {
+	Model
+	display_name string
+	email        string
+}
