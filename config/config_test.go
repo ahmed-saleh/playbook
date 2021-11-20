3668ebeb4a5e52da1d680d117c475d47168edc05
Ahmed Saleh <a.s.alsalali@gmail.com>
2021-11-20 16:56:22 +0800

    added tests for config package

---
+++ config/config_test.go
@@ -0,0 +1,31 @@
+package config_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	c "github.com/ahmed-saleh/playbook/config"
+)
+
+func TestPlaybook(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Config Suite")
+}
+
+var _ = Describe("Config", func() {
+
+	BeforeEach(func() {
+		c.Setup("test.ini")
+	})
+
+	Describe("Varifying DB setup", func() {
+		Context("Database Name", func() {
+			It("should be ", func() {
+				Expect(c.MysqlSettings.Name).To(Equal("playbook_test_db"))
+			})
+		})
+	})
+
+})
